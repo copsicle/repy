ec1916c2e43d65d10597865b1732314dfe9f5777
copsicle <gonen.cohen43@gmail.com>
2019-01-07 11:20:42 +0200

    preparations for PyPI upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="repy",
+    version="0.0.1",
+    author="Gonen C.",
+    author_email="gonen.cohen43@gmail.com",
+    description="The anti-repost bot for Reddit",
+    url="https://github.com/copsicle/repy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
